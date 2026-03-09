f8952f582426e2008f8048e49f648f8b70242420
ngdevelopment-tech <ngdevelopmentlb@gmail.com>
2026-03-09 17:37:50 +0200

    feat: complete exercises 2.1-2.5 rendering collections and modules

---
+++ part2/src/components/Course.jsx
@@ -0,0 +1,30 @@
+const Header = ({ name }) => <h2>{name}</h2>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => 
+      <Part key={part.id} part={part} />
+    )}
+  </div>
+)
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((total, part) => total + part.exercises, 0)
+  return <strong>total of {sum} exercises</strong>
+}
+
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+)
+
+export default Course
